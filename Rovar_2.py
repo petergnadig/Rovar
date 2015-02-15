b78268daef3f53cb5480af56ad5351f13f289928
petergnadig <pi@raspberrypi.(none)>
2015-02-15 12:36:47 +0000

    ...

--- Rovar_2.py
+++ Rovar_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
 from array import array
-import time, math
-import os, sys, inspect
+import time, math, os, sys, inspect
 
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"./Adafruit_PWM_Servo_Driver")))
 if cmd_subfolder not in sys.path:
